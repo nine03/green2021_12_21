d3c423aae06d59eeb20a518b62cfb07d66e85007
Yeongjun Kim <60682087+nine03@users.noreply.github.com>
2022-01-16 02:14:57 +0900

    Add files via upload

---
+++ src/day23_1/src/day23_1/Enemy.java
@@ -0,0 +1,20 @@
+package day23_1;
+import javax.swing.*;
+import java.awt.*;
+
+public class Enemy {
+    Image image = new ImageIcon("images/enemy.png").getImage();
+    int x, y;
+    int width = image.getWidth(null);
+    int height = image.getHeight(null);
+    int hp = 10;
+
+    public Enemy(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move() {
+         this.x -= 7;
+    }
+}
